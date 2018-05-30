2429b2d8a9799b134d69add87b325e503e2cff96
Thomas Burri <th@thonojato.ch>
2018-05-30 22:26:49 +0200

    Reordered sections in ini file. Updated TCConfig class Updated main window - now everything with editable comboboxes Updated main file to match new config parser class and new ui

--- src/tcconfig.py
+++ src/tcconfig.py
@@ -6,23 +6,49 @@ import sys
 import ConfigParser
 
 class TCConfig(object):
-    def __init__(self, configfilename):
+    TCC_CONFIGURATION = 0
+    TCC_LAYOUT = 1
+    TCC_MCG_FW_DIR_HIST = 2
+    TCC_MCG_FW_VERS_HIST = 3
+    TCC_MCG_PACK_DIR_HIST = 4
+    TCC_MCG_PACK_VERS_HIST = 5
+    TCC_INST_DST_HIST = 6
+    TCC_PACK_BRNCH_HIST = 7
+    TCC_PACK_TAG_HIST = 8
+    TCC_INST_SRC_HIST = 9
+    TCC_INST_DST_HIST = 10
+    TCC_SECTION_NAMES = {
+        TCC_CONFIGURATION:      'Configuration',
+        TCC_LAYOUT:             'Layout',
+        TCC_MCG_FW_DIR_HIST:    'McgFwDirHistory',
+        TCC_MCG_FW_VERS_HIST:   'McgFwVersionHistory',
+        TCC_MCG_PACK_DIR_HIST:  'McgPackDirHistory',
+        TCC_MCG_PACK_VERS_HIST: 'McgPackVersionHistory',
+        TCC_PACK_BRNCH_HIST:    'CreateMcgPackBranchHistory',
+        TCC_PACK_TAG_HIST:      'CreateMcgPackTagHistory',
+        TCC_INST_SRC_HIST:      'CopyToInstallSrcHistory',
+        TCC_INST_DST_HIST:      'CopyToInstallDstHistory',
+    }
+
+    def __init__(self, config_file_name):
         # read ini file
-        self.configfilename = configfilename
+        self.config_file_name = config_file_name
         self.config = ConfigParser.ConfigParser()
         self.config.optionxform = str
-        self.config.read(configfilename)
-        self.tc_config_meta = {'McgFwVersionHistory' = {'max_options' = 10},
-                               'McgFwDirHistory' = {'max_options' = 10},
-                               'McgPackDirHistory' = {'max_options' = 10},
-                               'McgPackVersionHistory' = {'max_options' = 10},
-                               'CreateMcgPackBranchHistory' = {'max_options' = 10},
-                               'CreateMcgPackTagHistory' = {'max_options' = 10},
-                               'CopyToInstallSrcHistory' = {'max_options' = 10},
-                               'CopyToInstallDstHistory' = {'max_options' = 10}}
+        self.config.read(config_file_name)
+        self.tc_config_meta = {
+            self.TCC_MCG_FW_DIR_HIST:  {'max_options': 10},
+            self.TCC_MCG_FW_VERS_HIST: {'max_options': 4},
+            self.TCC_MCG_PACK_DIR_HIST: {'max_options': 10},
+            self.TCC_MCG_PACK_VERS_HIST: {'max_options': 10},
+            self.TCC_PACK_BRNCH_HIST: {'max_options': 10},
+            self.TCC_PACK_TAG_HIST: {'max_options': 10},
+            self.TCC_INST_SRC_HIST: {'max_options': 10},
+            self.TCC_INST_DST_HIST: {'max_options': 10}
+        }
 
-    def saveConfig(self):
-        with open('toolsCollector_s.ini', 'wb') as configfile:
+    def saveConfig(self, config_file_name):
+        with open(config_file_name, 'wb') as configfile:
             self.config.write(configfile)
         
     def printOutConfig(self):
@@ -33,38 +59,52 @@ class TCConfig(object):
                 print '  %s = %s' % (name, value)
             print
 
-    def getSectionValues(self, section_name):
+    def getSectionValues(self, section_id):
         valueList = list()
-        for name, value in self.config.items(section_name):
+        for name, value in self.config.items(self.TCC_SECTION_NAMES[section_id]):
             valueList.append(value)
         return valueList
 
-    def getSectionFull(self, section_name):
+    def getSectionFull(self, section_id):
         sectionList = list()
-        for name, value in self.config.items(section_name):
+        for name, value in self.config.items(self.TCC_SECTION_NAMES[section_id]):
             sectionList.append((name, value))
         return sectionList
 
-    def updateSection(self, section_name, new_section_data):
+    def updateSection(self, section_id, new_section_data):
         option_counter = 0
-        max_options = self.tc_config_meta[section_name]['max_options']
-        for item in self.mcgFwVersions:
-            self.config.set(section_name, str(optionCounter), item)
-            optionCounter += 1
+        max_options = self.tc_config_meta[section_id]['max_options']
+        for data_value in new_section_data:
+            self.config.set(self.TCC_SECTION_NAMES[section_id], str(option_counter), data_value)
+            option_counter += 1
+            if option_counter >= max_options:
+                break
+            pass
 
-    
+
 if __name__ == "__main__":
     testConfig = TCConfig('toolscollector.ini')
-    # testConfig.printOutConfig()
-    sectionName = 'McgFwVersionHistory'
+    testConfig.printOutConfig()
+    sectionID = TCConfig.TCC_MCG_FW_VERS_HIST
 
-    sectionTouples = testConfig.getSectionFull(sectionName)
-    print 'Listing values of section %s' % sectionName
+    sectionTouples = testConfig.getSectionFull(sectionID)
+    print 'Listing values of section %s' % TCConfig.TCC_SECTION_NAMES[sectionID]
     for option, value in sectionTouples:
         print "%s=%s" % (option, value)
 
-    sectionValues = testConfig.getSectionValues(sectionName)
-    print 'Listing values of section %s' % sectionName
+    sectionValues = testConfig.getSectionValues(sectionID)
+    print 'Listing values of section %s' % TCConfig.TCC_SECTION_NAMES[sectionID]
     for value in sectionValues:
         print "value %s" % value
-    
+
+    # create test elements
+    sectionValues.insert(0, '3.22.0.x')
+    sectionValues.insert(0, '5.22.0.x')
+    sectionValues.insert(0, '4.22.0.x')
+    sectionValues.insert(0, '2.22.0.x')
+    sectionValues.insert(0, '2.21.0.x')
+    sectionValues.insert(0, '2.20.0.x')
+
+    testConfig.updateSection(sectionID, sectionValues)
+
+    testConfig.saveConfig('toolscollector_s.ini')
--- src/toolsCollector.py
+++ src/toolsCollector.py
@@ -3,11 +3,13 @@
 # during different actions.
 #
 import sys
-import ConfigParser
+from tcconfig import TCConfig
 
 from PyQt5.QtCore import pyqtSlot, QStringListModel
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUiType
+
+TOOLS_COLLECTOR_INI_FILE = 'toolscollector.ini'
 
 app = QApplication(sys.argv)
 form_class, base_class = loadUiType('mainwindow.ui')
@@ -16,34 +18,21 @@ class TCMainWindowImpl(QMainWindow, form_class):
     def __init__(self, *args):
         super(TCMainWindowImpl, self).__init__(*args)
         self.setupUi(self)
-        # read ini file
-        self.config = ConfigParser.ConfigParser()
-        self.config.optionxform = str
-        self.config.read('toolsCollector.ini')
-        for section_name in self.config.sections():
-            print 'Section:', section_name
-            print '  Options:', self.config.options(section_name)
-            for name, value in self.config.items(section_name):
-                print '  %s = %s' % (name, value)
-            print
 
-        self.mcgFwVersions = list()
-        for name, value in self.config.items('McgFwVersionHistory'):
-            self.mcgFwVersions.append(value)
-            self.cbMcgFwVersion.addItem(value)
-            # print '  %s = %s' % (name, value)
-        # self.cbPrevFwVers.insertItems(-1, self.mcgFwVersions)
-        # self.mcgFwVersionsModel = QStringListModel(self.mcgFwVersions, self)
-        # self.cbPrevFwVers.setModel(self.mcgFwVersionsModel)
+        # read ini file
+        self.config = TCConfig(TOOLS_COLLECTOR_INI_FILE)
+        self.mcg_fw_dirs = self.config.getSectionValues(TCConfig.TCC_MCG_FW_DIR_HIST)
+        self.mcg_fw_versions = self.config.getSectionValues(TCConfig.TCC_MCG_FW_VERS_HIST)
+        self.pack_dirs = self.config.getSectionValues(TCConfig.TCC_MCG_PACK_DIR_HIST)
+        self.pack_vers = self.config.getSectionValues(TCConfig.TCC_MCG_PACK_VERS_HIST)
+        self.pack_branches = self.config.getSectionValues(TCConfig.TCC_PACK_BRNCH_HIST)
+        self.pack_tags = self.config.getSectionValues(TCConfig.TCC_PACK_TAG_HIST)
+        self.inst_srcs = self.config.getSectionValues(TCConfig.TCC_INST_SRC_HIST)
+        self.inst_dsts = self.config.getSectionValues(TCConfig.TCC_INST_DST_HIST)
 
     @pyqtSlot()
     def on_app_aboutToQuit(self):
-        optionCounter = 0
-        for item in self.mcgFwVersions:
-            self.config.set('McgFwVersionHistory', str(optionCounter), item)
-            optionCounter += 1
-        with open('toolsCollector_s.ini', 'wb') as configfile:
-            self.config.write(configfile)
+        self.config.saveConfig(TOOLS_COLLECTOR_INI_FILE)
 
     @pyqtSlot(bool)
     def on_pbUpdateMcgFwVersions_clicked(self, checked):
@@ -52,33 +41,62 @@ class TCMainWindowImpl(QMainWindow, form_class):
         msg = "UpdateMcgFwVersions clicked with parameter = %s" % mcgFwVersion
         self.teLog.append(msg)
         self.teLog.append("-------------------------------------------------------------------------------\n")
-        if mcgFwVersion in self.mcgFwVersions:
+        if mcgFwVersion in self.mcg_fw_versions:
             # remove existing item and new added at front
-            self.mcgFwVersions.remove(mcgFwVersion)
-        self.mcgFwVersions.insert(0, mcgFwVersion)
+            self.mcg_fw_versions.remove(mcgFwVersion)
+        self.mcg_fw_versions.insert(0, mcgFwVersion)
+        self.config.updateSection(TCConfig.TCC_MCG_FW_VERS_HIST, self.mcg_fw_versions)
 
     @pyqtSlot(bool)
     def on_pbUpdateMcgPackVersions_clicked(self, checked):
+        mcgPackVersion = self.cbMcgPackVersions.currentText()
         self.teLog.append("===============================================================================")
-        msg = "UpdateMcgPackVersions clicked with parameter = %s" % self.leMcgPackVersions.text()
+        msg = "UpdateMcgPackVersions clicked with parameter = %s" % mcgPackVersion
         self.teLog.append(msg)
         self.teLog.append("-------------------------------------------------------------------------------\n")
+        if mcgPackVersion in self.pack_vers:
+            # remove existing item and new added at front
+            self.pack_vers.remove(mcgPackVersion)
+        self.pack_vers.insert(0, mcgPackVersion)
+        self.config.updateSection(TCConfig.TCC_MCG_PACK_VERS_HIST, self.pack_vers)
 
     @pyqtSlot(bool)
     def on_pbCreateMcgPackTags_clicked(self, checked):
+        brach_version = self.cbPackSrcBranchVersion.currentText()
+        tag_version = self.cbMcgPackTagVersion.currentText()
         self.teLog.append("===============================================================================")
-        msg = "CreateMcgPackTags clicked with packBranch = %s and packTag = %s" %\
-              (self.lePackSrcBranchVersion.text(), self.leMcgPackTagVersion.text())
+        msg = "CreateMcgPackTags clicked with packBranch = %s and packTag = %s" % (brach_version, tag_version)
         self.teLog.append(msg)
         self.teLog.append("-------------------------------------------------------------------------------\n")
+        if brach_version in self.pack_branches:
+            # remove existing item and new added at front
+            self.pack_branches.remove(brach_version)
+        self.pack_branches.insert(0, brach_version)
+        self.config.updateSection(TCConfig.TCC_PACK_BRNCH_HIST, self.pack_branches)
+        if tag_version in self.pack_tags:
+            # remove existing item and new added at front
+            self.pack_tags.remove(tag_version)
+        self.pack_tags.insert(0, tag_version)
+        self.config.updateSection(TCConfig.TCC_PACK_TAG_HIST, self.pack_tags)
 
     @pyqtSlot(bool)
     def on_pbCopyMcgFwToInstLoc_clicked(self, checked):
+        inst_src = self.cbMcgFwRepoVersion.currentText()
+        inst_dst = self.cbMcgFwInstVersion.currentText()
         self.teLog.append("===============================================================================")
-        msg = "CopyMcgFwToInstLoc clicked with src = %s and dst = %s" %\
-              (self.leMcgFwRepoVersion.text(), self.leMcgFwInstVersion.text())
+        msg = "CopyMcgFwToInstLoc clicked with src = %s and dst = %s" % (inst_src, inst_dst)
         self.teLog.append(msg)
         self.teLog.append("-------------------------------------------------------------------------------\n")
+        if inst_src in self.inst_srcs:
+            # remove existing item and new added at front
+            self.inst_srcs.remove(inst_src)
+        self.inst_srcs.insert(0, inst_src)
+        self.config.updateSection(TCConfig.TCC_INST_SRC_HIST, self.inst_srcs)
+        if inst_dst in self.inst_dsts:
+            # remove existing item and new added at front
+            self.inst_dsts.remove(inst_dst)
+        self.inst_dsts.insert(0, inst_dst)
+        self.config.updateSection(TCConfig.TCC_INST_DST_HIST, self.inst_dsts)
 
     @pyqtSlot(bool)
     def on_pbClearLog_clicked(self, checked):
