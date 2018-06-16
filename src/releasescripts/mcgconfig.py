8c9367226d82e32cc9b94b27d0510da7030d56ac
Thomas Burri <th@thonojato.ch>
2018-06-16 21:51:04 +0200

    updates now all master configurations correctly with given version

--- src/releasescripts/mcgconfig.py
+++ src/releasescripts/mcgconfig.py
@@ -25,66 +25,65 @@ def update_master_xml(file_name, old_firmware_version, new_firmware_version):
 def update_platform_xml(top_dir, new_firmware_version):
     result = True
     err_msg = 'No Error'
-    # root_dir = Path('/workspace/TWCS/03_mcg-dev/5_mcg-config/branches/RTC_CR_197559_TCNService_disabling')
     root_dir = Path(top_dir)
     rel_master_dir = Path('06_mcg_firmware/mcg_master_cfg')
-    # rel_master_dir = Path("__testdir\mcg_master_cfg")
-
     work_dir = root_dir.joinpath(rel_master_dir)
 
-    # version_nrtos1 = '3.22.2.0'
     version_nrtos1 = new_firmware_version
     version_vrue = version_nrtos1.split('.')
-    version_nrtos2 = '4.%s.%s.%s' % (version_vrue[1], version_vrue[1], version_vrue[3])
-    version_nrtos4 = '4.%s.%s.%s' % (version_vrue[1], version_vrue[1], version_vrue[3])
+    release_val = version_vrue[1]
+    update_val = version_vrue[2]
+    evolution_val = version_vrue[3]
+    version_nrtos2 = '4.%s.%s.%s' % (release_val, update_val, evolution_val)
+    version_nrtos4 = '4.%s.%s.%s' % (release_val, update_val, evolution_val)
     version_rub = version_nrtos1[version_nrtos1.find('.'):]
 
-    # replace mcg platform version
+    # update mcg platform files
     search_data = { 'mcg_platform_BT.xml' : '3', 'mcg2_platform_BT.xml' : '4', 'mcg4_platform_BT.xml' : '5'}
-    files_found = find_files(str(work_dir), '.*mcg.?_platform.*\.xml')
-    for file_path, file_name in files_found:
+    platform_files = find_files(str(work_dir), '.*mcg.?_platform.*\.xml')
+    for file_path, file_name in platform_files:
         print "File = %s\n" % file_name
         # for each platform master, search version line
         full_version = search_data[file_name] + version_rub
-        replace_in_next_line = False
-        for line in fileinput.input([file_path], inplace=1):
+        platform_files_input_obj = fileinput.input([file_path], inplace=1)
+        for line in platform_files_input_obj:
             lineno = string.find(line, '<SciMdluName>0000-mcg_stand_nrt')
             if lineno > 0:
-                replace_in_next_line = True
-            elif replace_in_next_line:
-                new_line_start = line[line.find('>'):]
+                # write current line then get next line
+                sys.stdout.write(line)
+                line = platform_files_input_obj.readline()
+                new_line_start = line[:line.find('>')+1]
                 new_line_end = line[line.rfind('<'):]
                 line = new_line_start + full_version + new_line_end
-                replace_in_next_line = False
             sys.stdout.write(line)
 
-    # for glob_pattern, stext, version_start in search_data:
-    #     full_version = version_start + "." + version_rub
-    #
-    #     for f in work_dir.glob(glob_pattern):
-    #         print f
-    #
-    #     pass
-    #     files_platform = [str(f) for f in work_dir.glob(glob_pattern)]
-    #     replace_in_next_line = False
-    #     if not files_platform == []:
-    #         for line in fileinput.input(files_platform, inplace=1):
-    #             lineno = string.find(line, stext)
-    #             if lineno > 0:
-    #                 replace_in_next_line = True
-    #             if replace_in_next_line:
-    #                 new_line_start = line[line.find('>'):]
-    #                 new_line_end = line[line.rfind('<'):]
-    #                 ine = new_line_start + full_version + new_line_end
-    #                 replace_in_next_line = False
-    #
-    #             sys.stdout.write(line)
+    master_files = find_files(str(work_dir), '.*mcg_(stand|master).*\.xml')
+    search_list = ['0000-mcg_base', '0000-mcg_framewrk', '0000-mcg_services']
+    for file_path, file_name in master_files:
+        print "File = %s\n" % file_name
+        file_input_obj = fileinput.input([file_path], inplace=1)
+        for line in file_input_obj:
+            # for each mcg master, search line with DLU name matching any of search_list
+            for key in search_list:
+                idx = string.find(line, key)
+                if idx > 0:
+                    sys.stdout.write(line)
+                    # write next four lines to file then replace with new release, update and evolution
+                    for counter in range(0, 3, 1):
+                        sys.stdout.write(file_input_obj.readline())
+                    file_input_obj.readline()
+                    sys.stdout.write('                <Release>%s</Release>\n' % release_val)
+                    file_input_obj.readline()
+                    sys.stdout.write('                <Update>%s</Update>\n' % update_val)
+                    file_input_obj.readline()
+                    sys.stdout.write('                <Evolution>%s</Evolution>\n' % evolution_val)
+                    line = file_input_obj.readline()
+                    break
+            sys.stdout.write(line)
 
-    files_platform = [filename for path, filename in files_found]
-    files_master = list(work_dir.glob('**/mcg_master*.xml'))
-    files_stand = list(work_dir.glob('**/0000-mcg_stand*.xml'))
-    files = [files_platform, files_master, files_stand]
-    print files
+    # files_master = list(work_dir.glob('**/mcg_master*.xml'))
+    # files_stand = list(work_dir.glob('**/0000-mcg_stand*.xml'))
+    # files = [files_master, files_stand]
 
     return result, err_msg
 
